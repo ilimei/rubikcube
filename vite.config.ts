ccfce4010a19162ce0103127b3dc98896f99e994
hugehard <514222057@qq.com>
2021-10-17 23:07:53 +0800

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: '/rubikcube',
   build: {
     outDir: 'docs',
   }
