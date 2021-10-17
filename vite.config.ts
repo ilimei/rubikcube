0090c8ed71abd134bb48dbbd456f31b651ca65bc
hugehard <514222057@qq.com>
2021-10-17 23:05:15 +0800

    移动输出文件夹

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  build: {
+    outDir: 'docs',
+  }
 })
